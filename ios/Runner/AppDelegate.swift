841f9b43bba612999d7ceb8ce921ff5a068e18c0
BobbyRyanHartono <bobbyryan692@gmail.com>
2026-08-31 13:16:05 +0700

    feat: add multi-layer screenshot, print screen, and image theft protection

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,10 +7,25 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    makeWindowSecure()
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
   func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
     GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+  }
+
+  private func makeWindowSecure() {
+    DispatchQueue.main.async {
+      if let window = self.window {
+        let field = UITextField()
+        field.isSecureTextEntry = true
+        window.addSubview(field)
+        field.centerYAnchor.constraint(equalTo: window.centerYAnchor).isActive = true
+        field.centerXAnchor.constraint(equalTo: window.centerXAnchor).isActive = true
+        window.layer.superlayer?.addSublayer(field.layer)
+        field.layer.sublayers?.first?.addSublayer(window.layer)
+      }
+    }
   }
 }
